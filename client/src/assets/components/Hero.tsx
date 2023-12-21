a69de24f1b29c2d6bf4f11eacd7b39ccbed161a5
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-21 14:03:43 +0100

    feat [client]: add hero image

--- client/src/assets/components/Hero.tsx
+++ client/src/assets/components/Hero.tsx
@@ -2,12 +2,20 @@ import Card from "./Card"
 import Typography from "./Typography"
 import Icon from "./Icon"
 
+import heroImage from "../images/hero-image.webp"
+
+
 const Hero = () => {
   return (
     <section className="grid grid-cols-2 gap-4 mt-8 sm:grid-cols-4 lg:grid-cols-6">
-      <Card className="flex flex-col justify-between col-span-2 sm:col-span-4 lg:row-span-2">
+      <Card className="relative flex flex-col justify-between col-span-2 overflow-hidden sm:col-span-4 lg:row-span-2">
         <Typography tag="h1" variant="h1">Popcorn Palace<br/><span className="text-white/70">Cinéma plain air</span></Typography>
         <Typography tag="p" variant="p" className="mt-28">Une ambiance chaleureuse<br/>sur un rooftop au cœur de<br/>Bruxelles.</Typography>
+        <img 
+          src={heroImage}
+          alt="Projection d'un film sur le rooftop Popcorn Palace" 
+          className="absolute object-cover min-h-full min-w-full -z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+        />
       </Card>
       <Card color="blue" className="flex flex-col gap-4 justify-center items-center text-center">
         <Icon name="headphones" className="w-16"></Icon>
