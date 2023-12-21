3a55c2d3d527d8c167bf2b0996e835e8267cc905
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-21 02:31:10 +0100

    refactor [client]: edit class variables

--- client/src/assets/components/Button.tsx
+++ client/src/assets/components/Button.tsx
@@ -8,36 +8,34 @@ interface ButtonProps {
   className?: string;
 }
 
-const Button = ({type, size, onClick, children, className }: ButtonProps) => {
-  let typeClasses, sizeClasses: string;
-  
+const Button = ({type, size, onClick, children, className }: ButtonProps) => {  
   switch (type) {
     case "primary":
-      typeClasses = "bg-white text-black hover:bg-white/85"
+      className += "bg-white text-black hover:bg-white/85"
       break;
     case "secondary":
-      typeClasses = "bg-white/10 text-white border border-white/10 hover:bg-white/15 hover:border-white/15"
+      className += "bg-white/10 text-white border border-white/10 hover:bg-white/15 hover:border-white/15"
       break;
     case "tertiary":
-      typeClasses = "bg-orange text-black hover:bg-orange/90"
+      className += "bg-orange text-black hover:bg-orange/90"
       break;
   }
 
   switch (size) {
     case "small":
-      sizeClasses = "text-sm px-4 py-2"
+      className += "text-sm px-4 py-2"
       break;
     case "medium":
-      sizeClasses = "text-base px-8 py-4"
+      className += "text-base px-8 py-4"
       break;
     case "round":
-      sizeClasses = "text-lg w-8 h-8 flex justify-center items-center"
+      className += "text-lg w-8 h-8 flex justify-center items-center"
       break;
   }
 
   return (
     <div>
-      <button onClick={onClick} className={`font-bold rounded-full active:scale-[.97] ${className} ${typeClasses} ${sizeClasses}`}>
+      <button onClick={onClick} className={`font-bold rounded-full active:scale-[.97] ${className}`}>
         {children}
       </button>
     </div>
