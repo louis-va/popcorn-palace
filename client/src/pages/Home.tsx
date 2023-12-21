5f8773d752230b7a50b04cba51cea66803b67d69
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-21 01:59:07 +0100

    feat [client]: add Nav component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Container from "../assets/components/Container"
+import Nav from "../assets/components/Nav"
 
 const Home = () => {
   return (
     <>
       <Container>
+        <Nav />
         <h1>Home</h1>
       </Container>
     </>
