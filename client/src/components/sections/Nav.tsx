fbfa1454f9c91d21abcc7e9a553b8f893860bdf1
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-26 02:38:36 +0100

    refactor [client]: improve login semantic

--- client/src/components/sections/Nav.tsx
+++ client/src/components/sections/Nav.tsx
@@ -1,39 +1,37 @@
 import { useState } from 'react';
 import { useAuth } from "../auth/useAuth";
 import Logo from "../common/Logo";
-import Button from "../common/Button"
+import Button from "../common/Button";
 import Login from "../auth/LoginModal";
 
 const Nav = () => {
   const { isLoggedIn, userData } = useAuth();
   const [isLoginOpen, setIsLoginOpen] = useState(false);
 
-  let accountBtn: JSX.Element;
+  const loggedInButton = (
+    <Button variant="tertiary" size="small" onClick={() => { console.log("account") }}>
+      <span className="inline-block w-3 h-3 mr-2 rounded-full bg-orange"></span>
+      {`${userData?.firstname} ${userData?.lastname}`}
+    </Button>
+  )
 
-  if (isLoggedIn) {
-    accountBtn = (
-      <Button variant="tertiary" size="small" onClick={()=>{console.log("account")}}>
-        <span className="inline-block w-3 h-3 mr-2 rounded-full bg-orange"></span> 
-        {`${userData?.firstname} ${userData?.lastname}`} 
-      </Button>
-    )
-  } else if ((isLoggedIn===false)) {
-    accountBtn = <Button variant="tertiary" size="small" onClick={()=>{setIsLoginOpen(true)}}> Connexion </Button>
-  } else {
-    accountBtn = <></>
-  }
+  const loggedOutButton = (
+    <Button variant="tertiary" size="small" onClick={() => setIsLoginOpen(true)}>
+      Connexion
+    </Button>
+  )
 
   return (
     <>
       <Login isOpen={isLoginOpen} setIsOpen={setIsLoginOpen} />
       <nav className="flex justify-between items-center h-20 sm:px-2">
         <a href="/" className="rounded text-orange hover:text-orange-dark">
-          <Logo/>
+          <Logo />
         </a>
-        {accountBtn}
+        {isLoggedIn ? (loggedInButton) : (loggedOutButton)}
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
