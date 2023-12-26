b1bc2be103778ef97981b5db52ff6a6af208c1ed
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-26 03:32:22 +0100

    refactor [client]: improve readability by separating fetch and types

--- client/src/components/sections/Screenings.tsx
+++ client/src/components/sections/Screenings.tsx
@@ -1,68 +1,49 @@
 import { useState, useEffect } from 'react';
-
 import Typography from "../common/Typography"
 import ScreeningCard from "../ui-elements/ScreeningCard"
-
-interface Screening {
-  movie: {
-    title: string;
-    poster: string;
-  },
-  _id: string;
-  date: Date;
-}
+import { IScreening } from '../../types/types';
+import { fetchScreenings } from '../../utils/fetchScreenings';
 
 const Screenings = () => {
-  const [screeningsData, setScreeningsData] = useState<Array<Screening> | null>(null);
+  const [screeningsData, setScreeningsData] = useState<IScreening[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const getScreenings = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/screenings`, 
-          { method: "GET" }
-        );
-        if (!response.ok) throw new Error('Failed to fetch data');
-
-        const screenings: Array<Screening> = await response.json();
-        const sorted = screenings
-          .map(screening => { return {...screening, date: new Date(screening.date)} })
-          .sort((a, b) => a.date.getTime() - b.date.getTime());
-        setScreeningsData(sorted);
+        const sortedScreenings = await fetchScreenings();
+        setScreeningsData(sortedScreenings);
+        setLoading(false);
       } catch (error: any) {
         setError(error.message || 'An error occurred');
-      } finally {
         setLoading(false);
       }
     };
-
-    getScreenings();
+  
+    fetchData();
   }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
 
   return (
     <section className="mt-12">
       <Typography as="h2" variant="h2">Nos prochaines projections</Typography>
-      <div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {screeningsData!.map((screening, index) => (
-          <ScreeningCard
-            key={index}
-            id={screening._id}
-            title={screening.movie.title}
-            poster={screening.movie.poster}
-            date={screening.date}
-          />
-        ))}
-      </div>
+      { loading ? (
+        <div className="mt-6">Chargement...</div>
+      ) : error ? (
+        <div className="mt-6">Une erreur s'est produite lors du chargement des séances.</div>
+      ) : (
+        <div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {screeningsData?.map((screening) => (
+            <ScreeningCard
+              key={screening._id}
+              id={screening._id}
+              title={screening.movie.title}
+              poster={screening.movie.poster}
+              date={screening.date}
+            />
+          ))}
+        </div>
+      )}
     </section>
   )
 }
---
+++ client/src/types/types.ts
@@ -0,0 +1,8 @@
+export interface IScreening {
+  movie: {
+    title: string;
+    poster: string;
+  },
+  _id: string;
+  date: Date;
+}
