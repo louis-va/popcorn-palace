8b92fd02c6acd82e17a13278ad1d002c80490322
Louis Van Aken <vanakenlouis@gmail.com>
2024-01-04 14:14:17 +0100

    refactor [client]: edit booking service files name

--- client/src/services/booking/checkBookingStatus.service.ts
+++ client/src/services/booking/checkBookingStatus.service.ts
@@ -1,17 +1,10 @@
-import { IBooking } from '@/types/types';
-
-export const createBooking = async (data: IBooking) => {
+export const checkBookingStatus = async (bookingId: string) => {
   try {
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
     const payload = JSON.stringify({
-      "screening_id": data.screening_id,
-      "seats": data.seats,
-      "tickets": data.tickets,
-      "price": data.tickets.reduce((sum, ticket) => sum + ticket.price, 0),
-      "success_url": `${import.meta.env.VITE_URL}/payment/confirmation`,
-      "cancel_url": `${import.meta.env.VITE_URL}/payment/confirmation`
+      "booking_id": bookingId
     });
 
     const options = {
@@ -24,7 +17,7 @@ export const createBooking = async (data: IBooking) => {
     // Simulating a 1 seconds delay
     await new Promise(resolve => setTimeout(resolve, 1000));
     
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/booking/create`, options);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/booking/status`, options);
 
     return response;
   } catch (error: any) {
--- client/src/services/booking/openBookingCheckout.service.ts
+++ client/src/services/booking/openBookingCheckout.service.ts
@@ -1,11 +1,17 @@
-export const validateBooking = async (bookingId: string, stripeSessionId: string) => {
+import { IBooking } from '@/types/types';
+
+export const openBookingCheckout = async (data: IBooking) => {
   try {
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
     const payload = JSON.stringify({
-      "booking_id": bookingId,
-      "session_id": stripeSessionId
+      "screening_id": data.screening_id,
+      "seats": data.seats,
+      "tickets": data.tickets,
+      "price": data.tickets.reduce((sum, ticket) => sum + ticket.price, 0),
+      "success_url": `${import.meta.env.VITE_URL}/payment/confirmation`,
+      "cancel_url": `${import.meta.env.VITE_URL}/payment/confirmation`
     });
 
     const options = {
@@ -18,7 +24,7 @@ export const validateBooking = async (bookingId: string, stripeSessionId: string
     // Simulating a 1 seconds delay
     await new Promise(resolve => setTimeout(resolve, 1000));
     
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/booking/validate`, options);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/booking/checkout`, options);
 
     return response;
   } catch (error: any) {
